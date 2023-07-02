a3931384360c64d042685351a5448f875f9d68d9
Andrew Qiao <andrewqiao2004@gmail.com>
2023-07-01 21:37:20 -0400

    comment category page back in

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -1,50 +1,47 @@
-export default function Category() {
-  return <div>Category</div>;
+import axios from 'axios';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { CATEGORIES } from '@/constants/categories';
+import { StockQuote } from '@/interfaces/interfaces';
+import StockList from '@/components/UI/StockList';
+
+export default function Category({ stocks }: { stocks: StockQuote[] }) {
+  const router = useRouter();
+  const { category } = router.query;
+
+  return (
+    <section className='mx-dashboard'>
+      <h1 className='mb-4 text-4xl font-semibold text-white 2xl:text-5xl'>
+        {CATEGORIES.find((c) => c.id === category)?.label}
+      </h1>
+      <StockList stocks={stocks} />
+    </section>
+  );
 }
 
-// import axios from 'axios';
-// import { GetStaticPaths, GetStaticProps } from 'next';
-// import { useRouter } from 'next/router';
-// import { CATEGORIES } from '@/constants/categories';
-// import { StockQuote } from '@/interfaces/interfaces';
-// import StockList from '@/components/UI/StockList';
+const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 
-// export default function Category({ stocks }: { stocks: StockQuote[] }) {
-//   const router = useRouter();
-//   const { category } = router.query;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  console.log(`${FRONTEND_BASE_URL}/api/stocks/categories`);
+  const { data } = await axios.get(
+    `${FRONTEND_BASE_URL}/api/stocks/categories`,
+    {
+      params: { type: params?.category },
+    }
+  );
+  return {
+    props: { stocks: data },
+    revalidate: 60,
+  };
+};
 
-//   return (
-//     <section className='mx-dashboard'>
-//       <h1 className='mb-4 text-4xl font-semibold text-white 2xl:text-5xl'>
-//         {CATEGORIES.find((c) => c.id === category)?.label}
-//       </h1>
-//       <StockList stocks={stocks} />
-//     </section>
-//   );
-// }
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = CATEGORIES.map((category) => ({
+    params: { category: category.id },
+  }));
 
-// const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { data } = await axios.get(
-//     `${FRONTEND_BASE_URL}/api/stocks/categories`,
-//     {
-//       params: { type: params?.category },
-//     }
-//   );
-//   return {
-//     props: { stocks: data },
-//     revalidate: 60,
-//   };
-// };
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const paths = CATEGORIES.map((category) => ({
-//     params: { category: category.id },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// };
+  return {
+    paths,
+    fallback: true,
+  };
+};
